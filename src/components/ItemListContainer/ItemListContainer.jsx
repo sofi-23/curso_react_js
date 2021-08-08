00a46cce002264c050c212736269c76d4defb4b9
Sofia Constenla <sofi.amc1995@gmail.com>
2021-08-07 22:16:14 -0300

    changing onAdd

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import ItemCount from './ItemCount';
+import {useState} from 'react';
 
 export default function ItemListContainer (props) {
+    const [itemCount, setItemCount ] = useState(0);
     return (
         <>
         <h1>{props.greeting}</h1>
-        <ItemCount stock={7}  initial={0} />
+        <ItemCount stock={7}  initial={0} onAdd={setItemCount} />
         </>
     )
 }
